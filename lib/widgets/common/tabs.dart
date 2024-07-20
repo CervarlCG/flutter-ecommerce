725e284a40aae3d01778c577afd62c74a1c3c50d
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-07-20 13:39:17 -0600

    Feat: Added custom tabs widget
    A widget for rendering custom tabs with custom styles was created

---
+++ lib/widgets/common/tabs.dart
@@ -0,0 +1,80 @@
+import 'package:ecommerce/models/theme/spacing.dart';
+import 'package:flutter/material.dart';
+
+class TabsContainer extends StatefulWidget {
+  const TabsContainer(
+      {required this.tabs, required this.getTabView, super.key});
+
+  final List<Tab> tabs;
+  final Widget Function(int index) getTabView;
+
+  @override
+  State<TabsContainer> createState() => _TabsContainerState();
+}
+
+class _TabsContainerState extends State<TabsContainer>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+  int _tabIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: widget.tabs.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _handleTabChange(int index) {
+    setState(() {
+      _tabIndex = index;
+    });
+  }
+
+  Widget? _getTabView() {
+    if (widget.tabs.isEmpty ||
+        _tabIndex < 0 ||
+        _tabIndex > widget.tabs.length) {
+      return null;
+    }
+    return widget.getTabView(_tabIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Column(children: [
+      TabBar(
+        controller: _tabController,
+        tabs: widget.tabs.map((item) {
+          return item;
+        }).toList(),
+        indicator: BoxDecoration(
+            color: theme.colorScheme.surface,
+            boxShadow: const <BoxShadow>[
+              BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 1,
+                  blurRadius: 20,
+                  offset: Offset(0, 3))
+            ],
+            borderRadius: BorderRadius.circular(spacing(1))),
+        indicatorPadding: EdgeInsets.symmetric(
+            vertical: spacing(0.5), horizontal: spacing(-1.5)),
+        indicatorSize: TabBarIndicatorSize.label,
+        overlayColor: WidgetStateColor.transparent,
+        dividerColor: Colors.transparent,
+        unselectedLabelColor: theme.colorScheme.secondary,
+        onTap: _handleTabChange,
+      ),
+      Container(
+        child: _getTabView(),
+      )
+    ]);
+  }
+}
