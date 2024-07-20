85a3f466f4d02bcfa09a25b7f85279a3ceca2de2
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-07-20 13:41:18 -0600

    Feate: ProductCard widget was created

---
+++ lib/widgets/ecommerce/product_card.dart
@@ -0,0 +1,65 @@
+import 'package:ecommerce/models/entities/product.dart';
+import 'package:ecommerce/models/entities/store.dart';
+import 'package:ecommerce/models/theme/spacing.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({required this.product, super.key});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Consumer<Store>(builder: (context, store, child) {
+      return Card(
+        key: Key(product.id),
+        shadowColor: Colors.transparent,
+        child: Padding(
+            padding: EdgeInsets.all(spacing(1)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AspectRatio(
+                  aspectRatio: 1 / 1,
+                  child: Stack(clipBehavior: Clip.none, children: [
+                    Container(),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(spacing(0.4)),
+                      child: Image(
+                        image: NetworkImage(product.image),
+                      ),
+                    )
+                  ]),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: spacing(0.5), bottom: spacing(0.5)),
+                    child: Text(product.name,
+                        style: TextStyle(color: theme.colorScheme.secondary))),
+                Row(
+                  children: [
+                    Text(
+                      store.money(product.price).toString(),
+                      style: theme.textTheme.labelLarge,
+                    ),
+                    const Spacer(),
+                    IconButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Center(child: Text("Test"))));
+                        },
+                        icon: Icon(
+                          Icons.shopping_cart,
+                          size: spacing(1),
+                        ))
+                  ],
+                )
+              ],
+            )),
+      );
+    });
+  }
+}
