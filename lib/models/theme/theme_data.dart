44b2bcf0b498a1eefb925f38a50b12935e8bcf26
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-07-20 13:40:13 -0600

    Feat: Main theme text and colors were added/updated

--- lib/models/theme/theme_data.dart
+++ lib/models/theme/theme_data.dart
@@ -5,17 +5,23 @@ const Color secondary = Colors.white;
 
 final ThemeData theme = ThemeData(
     colorScheme: const ColorScheme(
-        brightness: Brightness.light,
-        primary: primary,
-        onPrimary: secondary,
-        secondary: secondary,
-        onSecondary: primary,
-        error: Color.fromRGBO(244, 67, 54, 1),
-        onError: secondary,
-        surface: secondary,
-        onSurface: primary),
+      brightness: Brightness.light,
+      primary: primary,
+      onPrimary: secondary,
+      secondary: Color.fromRGBO(117, 117, 117, 1),
+      onSecondary: primary,
+      error: Color.fromRGBO(244, 67, 54, 1),
+      onError: secondary,
+      surface: secondary,
+      onSurface: primary,
+    ),
+    textTheme: const TextTheme(
+        titleMedium: TextStyle(fontWeight: FontWeight.w700),
+        titleLarge: TextStyle(fontWeight: FontWeight.w700),
+        labelLarge: TextStyle(fontWeight: FontWeight.w700)),
     useMaterial3: true,
     fontFamily: 'Montserrat',
+    scaffoldBackgroundColor: const Color.fromRGBO(245, 245, 245, 1),
     elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
             backgroundColor: primary,
