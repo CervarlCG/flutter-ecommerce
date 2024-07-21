e780829ca33306ddf8bf7eacc1bb455cbe0b9247
Jesus Cervantes <jesus.cervarl@gmail.com>
2024-07-21 11:09:29 -0600

    Feat: Replaces BottomNavigationBar
    - BottomNavigationView was replaced by native BottomNavigationBar
    - BottomNavigationView was removed from project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:ecommerce/models/entities/store.dart';
+import 'package:ecommerce/models/theme/spacing.dart';
 import 'package:ecommerce/models/theme/theme_data.dart';
 import 'package:ecommerce/screens/home.dart';
-import 'package:ecommerce/widgets/navigation/bottom_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:provider/provider.dart';
@@ -46,6 +46,14 @@ class EntryPage extends StatefulWidget {
 }
 
 class _EntryPageState extends State<EntryPage> {
+  int _selectedNavigationIndex = 0;
+
+  void _handleOnTapNavigationItem(int index) {
+    setState(() {
+      _selectedNavigationIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
@@ -56,21 +64,45 @@ class _EntryPageState extends State<EntryPage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      body: const HomeView(),
-      bottomNavigationBar: BottomNavigationView(
-        backgroundColor: theme.colorScheme.primary,
-        color: theme.colorScheme.onPrimary,
-        selectedBackgroundColor: theme.colorScheme.surface,
-        selectedColor: theme.colorScheme.onSurface,
-        items: [
-          BottomNavigationViewItem(icon: FeatherIcons.home, label: 'Home'),
-          BottomNavigationViewItem(
-              icon: FeatherIcons.shoppingBag, label: 'Shop'),
-          BottomNavigationViewItem(
-              icon: FeatherIcons.shoppingCart, label: 'Cart'),
-          BottomNavigationViewItem(icon: FeatherIcons.user, label: 'Account'),
-        ],
-      ),
-    );
+        body: const HomeView(),
+        bottomNavigationBar: Container(
+            decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(
+                        width: 1,
+                        color: theme.colorScheme.secondary.withOpacity(0.1)))),
+            child: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      FeatherIcons.home,
+                      size: spacing(1.3),
+                    ),
+                    label: 'Home'),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      FeatherIcons.shoppingBag,
+                      size: spacing(1.3),
+                    ),
+                    label: 'Shop'),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      FeatherIcons.shoppingCart,
+                      size: spacing(1.3),
+                    ),
+                    label: 'Cart'),
+                BottomNavigationBarItem(
+                    icon: Icon(FeatherIcons.user, size: spacing(1.3)),
+                    label: 'Account')
+              ],
+              selectedFontSize: 12,
+              selectedItemColor: theme.colorScheme.primary,
+              unselectedItemColor: theme.colorScheme.onSurface,
+              showUnselectedLabels: true,
+              backgroundColor: theme.colorScheme.surface,
+              type: BottomNavigationBarType.fixed,
+              currentIndex: _selectedNavigationIndex,
+              onTap: _handleOnTapNavigationItem,
+            )));
   }
 }
